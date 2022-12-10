ee004c173390eedb3e1bdc8392de8a2e1fa6443f
mnagaa <s.c.x0x0www10@icloud.com>
2022-12-11 05:06:41 +0900

    fix: invalid

--- terraform/project/common-snowflake/user.tf
+++ terraform/project/common-snowflake/user.tf
@@ -1,6 +1,6 @@
-resource "snowflake_user" "mnagaa" {
+resource "snowflake_user" "mnagaa2" {
   provider     = snowflake.user_admin
-  name         = "mnagaa"
+  name         = "mnagaa2"
   default_role = snowflake_role.developer.name
   comment      = "Created by Terraform."
 }

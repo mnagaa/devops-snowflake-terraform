3dedcb70d6f75d352032d9165cc8c871490148c3
mnagaa <s.c.x0x0www10@icloud.com>
2022-12-11 03:42:25 +0900

    feat: add setup

---
+++ terraform/project/common-snowflake/variable.tf
@@ -0,0 +1,12 @@
+variable "account_id" {
+  default = "065304511047"
+}
+variable "region" {
+  default = "ap-northeast-1"
+}
+variable "environment" {
+  default = "common"
+}
+variable "project" {
+  default = "mnagaa"
+}
---
+++ terraform/project/dev/snowflake/variable.tf
@@ -0,0 +1,9 @@
+variable "environment" {
+  default = "dev"
+}
+variable "project" {
+  default = "mnagaa"
+}
+variable "account_id" {
+  default = "0123456789" // AWSのアカウントid
+}

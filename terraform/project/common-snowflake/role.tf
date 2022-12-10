2a2be99005b463d017af3064899629ae0b72752a
mnagaa <s.c.x0x0www10@icloud.com>
2022-12-11 04:52:58 +0900

    feat: add role and user

---
+++ terraform/project/common-snowflake/role.tf
@@ -0,0 +1,38 @@
+locals {
+  snowflake_role = {
+    accountadmin  = "ACCOUNTADMIN"
+    sysadmin      = "SYSADMIN"
+    securityadmin = "SECURITYADMIN"
+    terraform     = "TERRAFORM" // WebUIから作成
+  }
+}
+
+// 開発者用のロール
+resource "snowflake_role" "developer" {
+  provider = snowflake.security_admin
+  name     = upper("developer")
+  comment  = "A role for developer."
+}
+
+// accountadminロールを利用できるユーザーを指定
+resource "snowflake_role_grants" "accountadmin" {
+  provider               = snowflake.security_admin
+  role_name              = local.snowflake_role.accountadmin
+  enable_multiple_grants = true
+  users = [
+    snowflake_user.mnagaa.name,
+  ]
+}
+
+resource "snowflake_role_grants" "developer" {
+  provider               = snowflake.security_admin
+  role_name              = snowflake_role.developer.name
+  enable_multiple_grants = true
+
+  roles = [local.snowflake_role.sysadmin]
+  // developerを付与するユーザを指定
+  users = [
+    snowflake_user.mnagaa.name,
+  ]
+}
+

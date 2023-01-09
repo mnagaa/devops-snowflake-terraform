4a7a85599a3c20ef26edb95787838e00e6d717b7
nagahama <s.c.x0x0www10@icloud.com>
2023-01-10 04:26:25 +0900

    feat: setup github actions workflow (#7)
    * feat: add gh actions
    
    * feat: add setup github actions
    
    * feat: add github actions workflow for terraform plan
    
    * feat: setup terraform-github
    
    * feat: add github actions workflow

---
+++ terraform/project/common-github/provider.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = "~> 1.3.0"
+
+  backend "s3" {
+    bucket         = "terraform-state-mnagaa"
+    region         = "ap-northeast-1"
+    key            = "common-github.tfstate"
+    dynamodb_table = "mnagaa-terraform-state-lock"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.47.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+  default_tags {
+    tags = {
+      env     = var.environment
+      project = var.project
+      owner   = "terraform"
+      tfstate = "common-github"
+    }
+  }
+}
---
+++ terraform/project/common-github/variable.tf
@@ -0,0 +1,8 @@
+variable "aws_account_id" {}
+variable "aws_region" {}
+variable "environment" {
+  default = "common"
+}
+variable "project" {
+  default = "mnagaa"
+}
